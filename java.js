14812702cf55463648e51475484a83eff4804bee
jilanijilani <162103591+jilanijilani@users.noreply.github.com>
2024-03-20 20:24:56 +0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,154 @@
+const wrapper = document.querySelector(".sliderwrapper")
+
+const menuitems = document.querySelectorAll(".menuitem")
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "air.png",
+      },
+      {
+        code: "darkblue",
+        img: "air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "jordan (1).png",
+      },
+      {
+        code: "green",
+        img: "jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "blazer.png",
+      },
+      {
+        code: "green",
+        img: "blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "hippie.png",
+      },
+      {
+        code: "black",
+        img: "hippie2.png",
+      },
+    ],
+  },
+];
+
+
+let choosenProduct = products[0];
+
+const currentProductimg = document.querySelector(".productimg");
+const currentProducttitle = document.querySelector(".producttitle");
+const currentProductprice = document.querySelector(".productprice");
+const currentProductcolors = document.querySelectorAll(".color");
+const currentProductsizes = document.querySelectorAll(".size");
+
+menuitems.forEach((item, index) =>  {
+  item.addEventListener("click", () => {
+    //change the current slide
+
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProducttitle.textContent = choosenProduct.title;
+    currentProductprice.textContent = "$" + choosenProduct.price;
+
+    currentProductimg.src = choosenProduct.colors[0].img;
+
+     //assing new colors
+     currentProductcolors.forEach((color,index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+
+   
+    
+    });
+  });
+});
+
+
+currentProductcolors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductimg.src = choosenProduct.colors[index].img;
+  });
+});
+
+
+
+
+
+
+currentProductsizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductsizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+
+const paybutton = document.querySelector(".productbutton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+paybutton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
+
+
+
